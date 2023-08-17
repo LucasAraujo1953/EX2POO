79347bfbdf8abe3a6bd2d395c238699d3f99ff4f
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-17 19:37:25 -0300

    Add files via upload

---
+++ EX2POO/EX2POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da aresta do quadrado: ");
+            double valorAresta = Convert.ToDouble(Console.ReadLine());
+
+            Quadrado quadrado = new Quadrado();
+            quadrado.Aresta = valorAresta;
+
+            double area = quadrado.CalcularArea();
+
+            Console.WriteLine($"A área do quadrado é: {area}");
+        }
+    }
+}
---
+++ EX2POO/EX2POO/Quadrado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2POO
+{
+    internal class Quadrado
+    {
+        private double aresta;
+
+        public double Aresta
+        {
+            get { return aresta; }
+            set { aresta = value; }
+        }
+
+        public double CalcularArea()
+        {
+            return aresta * aresta;
+        }
+    }
+}
